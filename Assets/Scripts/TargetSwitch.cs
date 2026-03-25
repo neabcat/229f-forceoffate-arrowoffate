124b2fa90ec01e6c566ffb9103f278b19784497a
Alicetear <phrnakanth@gmail.com>
2026-03-26 05:02:51 +0700

    Create interactive objects

--- Assets/Scripts/TargetSwitch.cs
+++ Assets/Scripts/TargetSwitch.cs
@@ -7,24 +7,17 @@ public class TargetSwitch : MonoBehaviour
     public Material newVisualMaterial;
     public PhysicsMaterial newPhysicMaterial;
     public GameObject cube;
-    public GameObject SpawnCubePrefab;
 
-    Vector3 pos;
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Physic target 1"))
+        if (collision.gameObject)
         {
             floorCollider.sharedMaterial = newPhysicMaterial;
             floorRenderer.material = newVisualMaterial;
-            pos = cube.transform.position;
             Destroy(cube.gameObject);
-
-            Invoke("SpawnCube", 10f);
         }
+        
     }
-    void SpawnCube()
-    {
-        cube = Instantiate(SpawnCubePrefab, pos, Quaternion.identity);
-    }
+   
 }
