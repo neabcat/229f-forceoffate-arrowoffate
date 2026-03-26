460e4a64418e1ca9eb0a30127e2f1fb906210a38
MalineeSunrise <malineebukong@gmail.com>
2026-03-27 00:38:03 +0700

    finishWall

--- Assets/Scripts/AcherTarget.cs
+++ Assets/Scripts/AcherTarget.cs
@@ -8,13 +8,21 @@ public class AcherTarget : MonoBehaviour
 
     public GameObject weight;
     public GameObject Wall;
+    public GameObject rock;
+
+    private void Start()
+    {
+        forceSpin = 0f;
+        force = 0f;
+        acceleration = 0f;
+    }
 
     void CalculateForce()
     {
         Rigidbody rb = weight.GetComponent<Rigidbody>();
         float mass = rb.mass;
         force = mass * acceleration;
-        rb.AddForce(new Vector3(0, force, force));
+        rb.AddForce(new Vector3(force, force, 0));
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,18 +36,39 @@ public class AcherTarget : MonoBehaviour
         }
         else if (gameObject.CompareTag("Target2"))
         {
+            forceSpin = 6000;
             Rigidbody rb = weight.GetComponent<Rigidbody>();
             rb.AddTorque(Vector3.up * forceSpin);
+
             Debug.Log("2");
         }
         else if (gameObject.CompareTag("Target3"))
         {
             acceleration = 600f;
             CalculateForce();
-
-            Destroy(Wall.gameObject, 10f);
             Debug.Log("3");
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.CompareTag("Rock"))
+        {
+            Debug.Log("Can");
+            force = 20;
+            Rigidbody rb = rock.GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.down * force);
+
+            Invoke("StopRock", 5f);
+        }
+        
+    }
+
+    private void StopRock()
+    {
+        Rigidbody rb = rock.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.isKinematic = true;
     }
 
 }
