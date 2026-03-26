c47484275ea46d7910ecc84a207d5137218b0614
NeabCAT <neabkub01@gmail.com>
2026-03-26 12:01:42 +0700

    Resolve Bow

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -3,31 +3,14 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     public Transform target;
-    public bool smoothFollow = true;
+    public bool smoothFollow = false;
     public float smoothSpeed = 20f;
 
     void LateUpdate()
     {
         if (target == null) return;
 
-        if (smoothFollow)
-        {
-            transform.position = Vector3.Lerp(
-                transform.position,
-                target.position,
-                smoothSpeed * Time.deltaTime
-            );
-
-            transform.rotation = Quaternion.Lerp(
-                transform.rotation,
-                target.rotation,
-                smoothSpeed * Time.deltaTime
-            );
-        }
-        else
-        {
-            transform.position = target.position;
-            transform.rotation = target.rotation;
-        }
+        transform.position = target.position;
+        transform.rotation = target.rotation;
     }
 }
