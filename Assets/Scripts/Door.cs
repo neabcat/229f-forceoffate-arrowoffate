6b8f6e150750489ed2da75f74446f4706990075a
Alicetear <phrnakanth@gmail.com>
2026-03-27 00:40:22 +0700

    create physic material floor

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Transform door;
+    public float speed = 2f;
+    private bool open = false;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Swicth"))
         {
-            Destroy(door.gameObject);
+            open = true;
         }
+    }
 
+    void Update()
+    {
+       if (open) 
+       {
+            door.position += Vector3.down * speed * Time.deltaTime;
+       } 
     }
 
 
