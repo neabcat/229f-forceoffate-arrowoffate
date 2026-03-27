db848f020998d2273973fca21f902fbac01e1b10
Alicetear <phrnakanth@gmail.com>
2026-03-27 08:12:50 +0700

    Create scripts puzzle and edit object

--- Assets/Scripts/InteractUIPuzzle.cs
+++ Assets/Scripts/InteractUIPuzzle.cs
@@ -5,24 +5,37 @@ public class InteractUIPuzzle : MonoBehaviour
     public Renderer[] blocks;   
     public Material[] colors;   
     public int[] answer = { 0, 1, 2 };
+    Material[] original;
 
     bool canInteract = false;
     int i = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        canInteract = true;
+    }
+
+    void Start()
+    {
+        original = new Material[blocks.Length];
+        for (int j = 0; j < blocks.Length; j++)
+        {
+            blocks[j].material = new Material(blocks[j].material);
+            original[j] = blocks[j].material;
+        }
+    }
 
     void Update()
     {
        
         if (canInteract  && Input.GetKeyDown(KeyCode.E))
         {
+            CancelInvoke();
             i = 0;
             Invoke("Next", 0f);
+            
         }
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        canInteract = true;
-    }
 
     void Next()
     {
@@ -31,13 +44,16 @@ public class InteractUIPuzzle : MonoBehaviour
             return;
         }
         blocks[i].material = colors[answer[i]];
-        Invoke("Hide", 1f);
+        Invoke("Hide", 0.5f);
     }
 
     void Hide()
     {
-        blocks[i].material = colors[0];
+        blocks[i].material = original[i];
         i++;
-        Invoke("Next", 2f);
+        if (i < answer.Length)
+        {
+          Invoke("Next", 1f);
+        }
     }
 }
